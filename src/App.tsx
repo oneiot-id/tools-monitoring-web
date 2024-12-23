3b80001520f27b1b0ac8acb4f93dc3f9c1f55251
vincentkenutama <vincentkenutama.2021@student.uny.ac.id>
2024-12-23 20:32:17 +0700

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-      </Routes>
-    </BrowserRouter>
+    <LoginPage/>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<LoginPage/>}/>
+    //     <Route path="/dashboard" element={<Dashboard/>}/>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
